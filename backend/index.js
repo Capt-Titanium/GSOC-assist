5aab7b12cb082dd86943ddcbe28e99a628b25dab
Tanish Agarwal <tanishagarwal81201@gmail.com>
2021-01-11 15:02:30 +0530

    updating changes from upstream

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,27 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const connectDb = require('./configs/dbConfig')
-const User = require('./models/user')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDb = require("./configs/dbConfig");
+const User = require("./models/user");
 
 // Requiring routes
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require("./routes/authRoutes");
 
-const app = express()
+const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
-connectDb()
+connectDb();
 
-app.use(cors())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Use routes
-app.use(authRoutes)
+app.use(authRoutes);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`App is listening on PORT: ${PORT}`)
-})
+  console.log(`App is listening on PORT: ${PORT}`);
+});
